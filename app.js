f35059f301023f0bdfa2127c670214666948bd51
eagonzalez81 <gonzoatc81@gmail.com>
2022-05-28 17:04:54 -0400

    add render method

--- app.js
+++ app.js
@@ -13,17 +13,59 @@ const game = {
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
     (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    do {
+      getGuess()
+      this.prevGuesses.push(this.userGuess)
+      render()
+    } while (this.userGuess !== this.secretNum) 
   },
+  
+  
+  
+  
+  
   getGuess: function() {
-    userGuess = prompt(`Enter a guess between ${smallestNum} and ${biggestNum}.}`)
-    if (this.userGuess > this.secretNum) {
-
+    parseInt(userGuess) = prompt(`Enter a guess between ${smallestNum} and ${biggestNum}. (Previous guesses are ${this.prevGuesses}`)
+    while (this.userGuess !== this.secretNum) {
+    if (this.userGuess > this.biggestNum) {
+      prompt('Number too big!')
+    } else if (this.userGuess < this.smallestNum) {
+      prompt('Number too small!')
+    } else if (this.userGuess > this.secretNum) {
+      this.biggestNum = userGuess
+    } else if (this.userGuess < this.secretNum) {
+      this.smallestNum = userGuess
+    } else {
+      prompt(`Congrats! You guessed the number in [number of prevGuesses]!`)
     }
   }
-  
-  
+},
+
+
+  render: function() {
+    if (this.userGuess > this.secretNum) {
+        alert(`Your guess is too high, previous guesses ${this.prevGuesses}`)
+      } else if (this.userGuess < this.secretNum) {
+        alert(`Your guess is too low, previous guesses: ${this.prevGuesses}`)
+      } else if (this.userGuess === this.secretNum) {
+        alert(`Your guess is too low, previous guesses ${join(this.prevGuesses)}`)
+      } else {
+        alert(`Congrats!You guessed the number in [number of prevGuesses]!`)
+  }
+}
 }
 
+
+
+// if (this.userGuess > this.secretNum) {
+//   alert(`Your guess is too high, previous guesses ${this.prevGuesses}`)
+// } else if (this.userGuess < this.secretNum) {
+//   alert(`Your guess is too low, previous guesses ${this.prevGuesses}`)
+// } else if (this.userGuess === this.secretNum) {
+//   alert(`Your guess is too low, previous guesses ${this.prevGuesses}`)
+// } else {
+//   alert('Congrats!')
+// }
 
 
 // game.prevGuesses = []
@@ -59,4 +101,4 @@ const game = {
 
 
 
-console.log(game)
+// console.log(game)
