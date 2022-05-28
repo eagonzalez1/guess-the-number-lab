24874f146a0c5f8e3ce8b071a45355d6c7cd6bd4
eagonzalez81 <gonzoatc81@gmail.com>
2022-05-28 15:57:47 -0400

    add guess method

--- app.js
+++ app.js
@@ -5,14 +5,57 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  userGuess: null,
+  prevGuesses: [],
+  
+  
+  
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+  },
+  getGuess: function() {
+    userGuess = prompt(`Enter a guess between ${smallestNum} and ${biggestNum}.}`)
+    if (this.userGuess > this.secretNum) {
+
+    }
   }
+  
+  
 }
 
-game.prevGuesses = []
 
+
+// game.prevGuesses = []
+
+
+// pla: function() {
+
+// }
+
+// let guess
+// guess = prompt(`hey ${numer}`)
+
+
+// let guess = ''
+// while (guess !== 'quit') {
+//   guess = prompt(`Enter a guess between ${smallestNum} and ${biggestNum}.`)
+//   if (guess < secretNum) {
+//     let price = catalog[sku]
+//     alert(`Your guess is too low, previous guesses ${prevGuesses}`)
+//   } else if (guess > secretNum) {
+//     alert(`Your guess is too high, previous guesses ${prevGuesses}`)
+//   }
+// }
+
+
+
+// function getGuess() {
+//   let guess = prompt(`Enter a guess between ${smallestNum} and ${biggestNum}.`)
+// }
+
+// let guess = ''
+// while (guess !== )
 
 
 
